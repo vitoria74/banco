52a8c7e8214eb4cc260ab3b65f9986ea90256931
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-29 14:36:44 -0300

    herança implementada

---
+++ src/Conta.java
@@ -0,0 +1,31 @@
+public class Conta {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+    
+}
--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,18 +1,4 @@
-public class ContaPoupanca {
-    
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    public void sacar(){
-
-    }
-
-    public void depositar(){
-
-    }
-
-    public void transferir(){
-        
-    }
+public class ContaPoupanca extends Conta{
 }
+
+    
