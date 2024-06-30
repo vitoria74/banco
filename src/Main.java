cfde1101e3c67aadb75496b7e95ceb54eda2472d
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-30 15:29:50 -0300

    polimorfismo aplicado

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     
     public static void main(String[] args) { // criando duas contas 
 
-        Conta corrente = new ContaCorrente();
+        Conta corrente = new ContaCorrente(); // polimorfismo: posso colocar como Conta, ContaCorrente, IConta....
         corrente.depositar(100);
 
         Conta poupanca = new ContaPoupanca();
