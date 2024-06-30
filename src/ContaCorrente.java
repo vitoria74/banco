a579e55ff1b5bf780345d892748840c9ac0ad347
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-30 14:18:16 -0300

    imprimirExtrato() refatorado -> imprimirAtributos() em ContaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -3,6 +3,10 @@ public class ContaCorrente extends Conta{
     @Override
     public void imprimirExtrato() {
         System.out.println("=== EXTRATO CONTA CORRENTE ===");
+        imprimirAtributos();
+    }
+
+    protected void imprimirAtributos() {
         System.out.println(String.format("Agência: %d", super.agencia));
         System.out.println(String.format("Número: ", super.numero));
         System.out.println(String.format("Saldo: %.2f", super.saldo));
