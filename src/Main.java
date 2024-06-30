9fd458e2d43beeed59d4dbdd24fd5f7f6163a42b
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-30 15:42:43 -0300

    Cliente adicionado a Main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     
     public static void main(String[] args) { // criando duas contas 
 
-        Conta corrente = new ContaCorrente(); // polimorfismo: posso colocar como Conta, ContaCorrente, IConta....
+        Cliente vitoria = new Cliente();
+        vitoria.setNome("Vitória");
+
+        Conta corrente = new ContaCorrente(vitoria); // polimorfismo: posso colocar como Conta, ContaCorrente, IConta....
         corrente.depositar(100);
 
-        Conta poupanca = new ContaPoupanca();
+        Conta poupanca = new ContaPoupanca(vitoria);
         corrente.transferir(100, poupanca);
 
         corrente.imprimirExtrato();
