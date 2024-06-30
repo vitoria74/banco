4e9cb739c6a3e6cbc5e131889569a045b127ad9d
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-30 15:20:22 -0300

    main teste2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) { // criando duas contas 
 
         Conta corrente = new ContaCorrente();
+        corrente.depositar(100);
+
         Conta poupanca = new ContaPoupanca();
+        corrente.transferir(100, poupanca);
 
         corrente.imprimirExtrato();
         poupanca.imprimirExtrato();
