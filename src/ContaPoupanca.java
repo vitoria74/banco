acd877670cca042211ff4bbd35930164a15ca190
vitoria74 <maria_vitoria74@hotmail.com>
2024-07-01 15:14:12 -0300

    javadoc ContaPoupanca

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,9 +1,20 @@
+/**
+ *Classe para lidar com a conta poupança. Herda a classe Conta.
+ */
+
 public class ContaPoupanca extends Conta{
 
+    /**
+     * Construtor da classe ContaPoupanca
+     * @param cliente - Nome do titular
+     */
     public ContaPoupanca(Cliente cliente) {
         super(cliente);
     }
 
+    /**
+     *  Método para imprimir as informações do extrato bancário da poupança
+     */
     @Override
     public void imprimirExtrato() {
         System.out.println("=== EXTRATO CONTA POUPANCA ===");
