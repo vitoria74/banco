720401ea296c5b7469c3dda9ba75df4de40e0feb
vitoria74 <maria_vitoria74@hotmail.com>
2024-06-30 21:41:40 -0300

    lombok em Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,12 +1,8 @@
+import lombok.Data;
+
+@Data
+
 public class Cliente {
     
     private String nome;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
