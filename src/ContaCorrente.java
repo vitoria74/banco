863671f3008543cb088353f2cdf0d89a79fea098
vitoria74 <maria_vitoria74@hotmail.com>
2024-07-01 15:30:19 -0300

    javadoc ContaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,9 +1,20 @@
+/**
+ * Classe para lidar com a conta corrente. Herda a classe Conta.
+ */
+
 public class ContaCorrente extends Conta{
 
+    /**
+     * Construtor de ContaCorrente
+     * @param cliente - Nome do titular
+     */
     public ContaCorrente(Cliente cliente) {
         super(cliente);
     }
 
+    /**
+     *  Método para imprimir as informações do extrato bancário da conta corrente
+     */
     @Override
     public void imprimirExtrato() {
         System.out.println("=== EXTRATO CONTA CORRENTE ===");
